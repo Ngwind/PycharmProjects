28ef0f85cb54b6a3211c8a5c32d077ace329a108
Ngwind <137476684@qq.com>
2018-09-19 18:40:49 +0800

    upload learning library: selenium
    Signed-off-by: Ngwind <137476684@qq.com>

---
+++ learn-selenium/first-selenium.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+'''
+这是一个selenium的小例子
+- 打开python.org网站 ，断言页面标题是否为Python
+- 在search框输入pycon，断言是否有搜索结果
+- 最后关闭浏览器
+'''
+
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No result found." not in driver.page_source
+driver.close()
+
